edba5cde5d1d0745a25a0d6b8b4441ba65b1f00f
Derya Yavuz <deryavuz@seas.upenn.edu>
2019-07-22 12:53:55 -0400

    modified and cleaned up

--- performance_analysis_functions.py
+++ performance_analysis_functions.py
@@ -3,100 +3,12 @@ import matplotlib.pyplot as plt
 import os, sys
 import numpy as np
 
-# def produce_df(file_name):
-#     """
-
-#     """
-#     data = pd.read_csv(file_name)
-    
-
- 
-
-
-#     # Adding another column to indicate the number of trials that has passed since the last source switch occurred.
-#     positionCol = [1] * data.count()
-#     data['posSinceCP'] = positionCol
-    
-#     # Add the hazard rate column. 
-#     # Load the sim_data.csv file.
-#     simulation = os.path.expanduser(
-#             '~/Documents/GitHub/Analysis_Audio2AFC_ChangePoint/sim_data.csv'
-#     )
-    
-#     simulation_data = pd.read_csv(simulation)
-#     data = data.merge(simulation_data['hazard'], left_index=True, right_index=True)
-    
-#     # Iterate over the rows in the source, checking the previous row and comparing it to the first one to see if there is 
-#     # a source switch.
-#     # This fills out the sourceSwitch column.
-
-#     data['sourceChange'] = data['source'].shift(1)
-#     data['sourceSwitch'] = data['source'] != data['sourceChange']
-    
-   
-#     # Iterate over the rows in the sourceSwitch column and fill the posSinceCP column. 
-#     #If there has been a switch, set the counter to 0, else, keep on iterating the counter. 
-    
-#     for row_number in range(len(data)):
-
-#         isSwitch = data.iloc[row_number, 'sourceSwitch']
-#         if isSwitch:
-#             count_switches = 0
-#         else:
-#             count_switches+=1
-#         data.iloc[row_number, 'posSinceCP'] = count_switches
-        
-#     return data
-
-
-# def extract_hazard(data):
-#     """
-    
-#     """
-#     # Finding the maximum value in the posSinceCP column to find the range of iterations.
-
-#     high_hazard_extracted = data[data['hazard'] == 0.9].copy()
-#     low_hazard_extracted = data[data['hazard'] == 0.1].copy()
-    
-    
-
-
-        
-#     max_value_high = high_hazard_extracted['posSinceCP'].max()
-#     max_value_low = low_hazard_extracted['posSinceCP'].max()
-    
-    
-#     #List to keep track of the percentage correct per value of the position counter.
-#     list_of_percentages_low = []
-#     list_of_percentages_high = []
-
-#     # Calculates % correct per each value of position counter: number of correct answers / number of trials (rows).
-#     # For high hazard rate.
-#     for val in range(0, int(max_value_high+1)):
-
-#         extracted_df_high = data[(data['posSinceCP'] == val) & (data['hazard']==0.9)].copy()
-#         percent_correct_high = extracted_df_high['correct'].sum() / len(extracted_df_high)
-#         list_of_percentages_high.append(percent_correct_high)
-        
-
-#     # Calculates % correct per each value of position counter: number of correct answers / number of trials (rows).
-#     # For low hazard rate.
-#     for val in range(0, int(max_value_low+1)):
-
-#         extracted_df_low = data[(data['posSinceCP'] == val) & (data['hazard']==0.1)].copy()
-#         percent_correct_low = extracted_df_low['correct'].sum() / len(extracted_df_low)
-#         list_of_percentages_low.append(percent_correct_low)
-        
-#     vector_high = np.arange(0,(max_value_high+1))
-#     vector_low = np.arange(0, (max_value_low+1))
-    
-        
-#     return list_of_percentages_high, list_of_percentages_low, vector_high, vector_low
 
 
 def get_timestamp(dataframe, subject, task_type):
     timestamp = dataframe[(dataframe['subject']==subject) & (dataframe['block']==task_type)]
     return timestamp.iloc[0,0]
+
 
 def build_filename(task_type, subject):
     
@@ -122,6 +34,7 @@ def build_filename(task_type, subject):
      
     return data
 
+
 def load_trial_seq():
     
     simulation = os.path.expanduser(
@@ -131,20 +44,12 @@ def load_trial_seq():
     simulation_data = pd.read_csv(simulation)
     return simulation_data
 
+
 def remove_cols(df):
     df = df.drop(["trialStart", "trialEnd", "RT", "direction", "isCatch", "unselectedTargetOff", "sound1On", "sound2On", 
                            "sound1Off", "sound2Off", "choiceTime", "secondChoiceTime", "targetOff", "fixationOff", "dirReleaseChoiceTime"], axis=1)
     return df
 
-# def add_switch_col(df):
-    
-#     # Adding a column to indicate source switches.
-# #     df['sourceSwitch'] = np.nan
-# #     df['sourceShift'] = df['source'].shift(1)
-# #     df['sourceSwitch'] = df['source'] != df['sourceShift']
-# #     df = df.drop(['sourceShift'], axis=1)
-
-#     return df
 
 def compute_pos_data(data):
     
@@ -193,6 +98,7 @@ def add_posSinceCP(df):
     return df
 
 
+
 def extract_hazard(df, h):
     
     hazard_extracted = df[df['hazard'] == h].copy()
@@ -202,13 +108,12 @@ def extract_hazard(df, h):
 def compute_percent(df):
     
     max_value = df['posSinceCP'].max()
-         #List to keep track of the percentage correct per value of the position counter.
-        
+    
+    #List to keep track of the percentage correct per value of the position counter.
     list_of_percentages = []
 
 
     # Calculates % correct per each value of position counter: number of correct answers / number of trials (rows).
-    # For high hazard rate.
     for val in range(0, int(max_value+1)):
 
         extracted_df = df[df['posSinceCP'] == val].copy()
